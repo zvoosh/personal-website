123647718d7bb794351a82c047db26d71098556a
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-11-21 15:30:59 +0100

    fix: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import { NewHome } from './components/NewHome';
-import { HeaderPage } from './layout/HeaderPage';
-import { Profile } from './components/Profile';
+// import { Routes, Route } from 'react-router-dom';
+// import { NewHome } from './components/NewHome';
+// import { HeaderPage } from './layout/HeaderPage';
+// import { Profile } from './components/Profile';
 
 
 function App() {
 
   return (
-    <div className='workSpace'>
-      <div className='headerSpace'>
-        <HeaderPage />
-      </div>
-      <div className='contentSpace'>
-        <Routes>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/" element={<NewHome />} />
-        </Routes>
-      </div>
-    </div>
+    <div>DUSAN</div>
+    // <div className='workSpace'>
+    //   <div className='headerSpace'>
+    //     <HeaderPage />
+    //   </div>
+    //   <div className='contentSpace'>
+    //     <Routes>
+    //       <Route path="/profile" element={<Profile />} />
+    //       <Route path="/" element={<NewHome />} />
+    //     </Routes>
+    //   </div>
+    // </div>
   )
 }
 
