75eed154299752b47a0f39f7a793b9ef86f18f9c
Dusan Ilic <dusan.ilic1999@gmail.com>
2023-12-06 12:24:54 +0100

    fix: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import './App.css'
 import { Header } from './components/Header';
 import { NewHome } from './components/NewHome';
